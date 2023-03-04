51ce180b445aa94ba32cb04d772c952a83637f0b
Jhoni ipia <jhonieripianoscue@gmail.com>
2023-03-03 19:12:19 -0500

    cambio hecho el 03 de marzo del 2023 a las 07:12 pm

---
+++ ubicaciones/ubicaciones.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function ubicaciones() {
+  return (
+    <div>
+     <table>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Dirección</th>
+            <th>Teléfono</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+     </table>
+      
+    </div>
+  )
+}
+
+export default ubicaciones
